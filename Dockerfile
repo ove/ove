fd0ca3be52e5922787c809cfb6715e3738647d41
Ovidiu Serban <serban.ovidiu.m@gmail.com>
2018-10-29 16:18:02 +0000

    versioned builds

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,10 @@
 FROM node:10-alpine
 WORKDIR /usr/src/app
 
-COPY . .
-
 RUN npm install -global pm2
 RUN npm install -global lerna
 
+COPY . .
 RUN npm run install:prod 
 
 RUN npm uninstall -global lerna
---
+++ build.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+scriptPath=$(dirname "$(readlink -f "$0")")
+cd ${scriptPath}/
+
+function deactivate_env {
+  echo "Deactivating env variables ..."
+  unset SERVICE_VERSION
+}
+
+function display_help() {
+  echo "Build image for ove"
+  echo
+  echo "usage: build.sh [option]..."
+  echo "   --push            Push the image"
+  echo "   -v, --version     The version of the service"
+}
+
+version=`git describe --tags --exact-match 2> /dev/null`
+if [ $? -ne 0 ]; then
+  version="latest"
+fi
+
+pushImage=false
+
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  case $key in
+    -h|--help)
+      display_help
+      exit 0
+      ;;
+    -v|--version)
+      version="$2"
+      echo "Recognized user version = ${version}"
+      shift
+      ;;
+    --push)
+      pushImage=true
+      ;;
+    *)
+      echo "Unrecognised option: $key"
+      echo
+      display_help
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+export SERVICE_VERSION=${version}
+trap deactivate_env EXIT SIGINT SIGTERM
+
+echo "Building version = ${SERVICE_VERSION}"
+
+docker-compose build
+if [ $? -ne 0 ]; then
+  # fail if the image build process failed
+  exit 1
+fi
+
+if [ "${pushImage}" = true ]; then
+  docker-compose push
+fi
