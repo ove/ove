48121a84d0b5142d9a4d29573795d5352e9d5ed8
Senaka Fernando <senaka@wso2.com>
2018-12-12 20:09:13 +0000

    Polyfills for OVE to support some older browser versions.

--- packages/ove-core/src/client/utils/utils.js
+++ packages/ove-core/src/client/utils/utils.js
@@ -24,6 +24,40 @@ function OVEUtils () {
     const log = this.Logger('OVEUtils');
 
     function OVELogger (name, logLevel) {
+        //-- https://github.com/uxitten/polyfill/blob/master/string.polyfill.js --//
+        //-- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart --//
+        if (!String.prototype.padStart) {
+            String.prototype.padStart = function padStart (targetLength, padString) {
+                targetLength = targetLength >> 0;
+                padString = String(typeof padString !== 'undefined' ? padString : ' ');
+                if (this.length >= targetLength) {
+                    return String(this);
+                } else {
+                    targetLength = targetLength - this.length;
+                    if (targetLength > padString.length) {
+                        padString += padString.repeat(targetLength / padString.length);
+                    }
+                    return padString.slice(0, targetLength) + String(this);
+                }
+            };
+        }
+        //-- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padEnd --//
+        if (!String.prototype.padEnd) {
+            String.prototype.padEnd = function padEnd (targetLength, padString) {
+                targetLength = targetLength >> 0;
+                padString = String((typeof padString !== 'undefined' ? padString : ' '));
+                if (this.length > targetLength) {
+                    return String(this);
+                } else {
+                    targetLength = targetLength - this.length;
+                    if (targetLength > padString.length) {
+                        padString += padString.repeat(targetLength / padString.length);
+                    }
+                    return String(this) + padString.slice(0, targetLength);
+                }
+            };
+        }
+
         //-- Constants for log labels corresponding to levels.  --//
         const LOG_PREFIX_WIDTH = 9;
 
