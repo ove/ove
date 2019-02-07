7f20f0871c90a6cdf4e706f1df265739dfff3e58
James Scott-Brown <james@jamesscottbrown.com>
2019-02-07 18:48:57 +0000

    more tests

--- packages/ove-lib-filter/test/core-functionality.test.js
+++ packages/ove-lib-filter/test/core-functionality.test.js
@@ -1,32 +1,13 @@
-/*
-const path = global.path;
-const fs = global.fs;
-const request = global.request;
-const express = global.express;
-const mockHttp = global.mockHttp;
-const HttpStatus = global.HttpStatus;
-const srcDir = global.srcDir;
-const index = global.index;
-const dirs = global.dirs;
-const Utils = global.Utils;
-*/
-
-
 // We always test against the distribution not the source.
 const path = require('path');
 const srcDir = path.join(__dirname, '..', 'lib');
 const getPredicate = require(path.join(srcDir, 'filter')).getPredicate;
 const parse = require(path.join(srcDir, 'parser')).parse;
 
-
-// arithmetic operators
-
-
-
-describe('The OVE filter library', () => {
+describe('The OVE filter library - logical operations', () => {
     const OLD_CONSOLE = global.console;
     beforeAll(() => {
-       // global.console = { log: jest.fn(x => x), warn: jest.fn(x => x), error: jest.fn(x => x) };
+        global.console = { log: jest.fn(x => x), warn: jest.fn(x => x), error: jest.fn(x => x) };
     });
 
     // Inequalities and equalities
@@ -44,14 +25,12 @@ describe('The OVE filter library', () => {
         expect(predicate({ x: '10' })).toBe(true);
     });
 
-
     it('should correctly handle "lt" (<)', () => {
         const predicate = getPredicate(parse('x lt 5'));
         expect(predicate({ x: '1' })).toBe(true);
         expect(predicate({ x: '5' })).toBe(false);
         expect(predicate({ x: '10' })).toBe(false);
     });
-
 
     it('should correctly handle "le" (<=)', () => {
         const predicate = getPredicate(parse('x le 5'));
@@ -61,12 +40,10 @@ describe('The OVE filter library', () => {
     });
 
     it('should correctly handle "eq" (=)', () => {
-        // TODO: types
         const predicate = getPredicate(parse('x eq 5'));
         expect(predicate({ x: '1' })).toBe(false);
         expect(predicate({ x: '5' })).toBe(true);
         expect(predicate({ x: '10' })).toBe(false);
-
     });
 
     it('should correctly handle "ne" (!=)', () => {
@@ -77,7 +54,6 @@ describe('The OVE filter library', () => {
     });
 
     // Logical operators
-
     it('should correctly handle "and"', () => {
         const predicate = getPredicate(parse('x and y'));
         expect(predicate({ x: true, y: true })).toBe(true);
@@ -94,20 +70,24 @@ describe('The OVE filter library', () => {
         expect(predicate({ x: false, y: false })).toBe(false);
     });
 
-    /*
     it('should correctly handle "not"', () => {
         const predicate = getPredicate(parse('not x'));
         expect(predicate({ x: true, y: true })).toBe(false);
         expect(predicate({ x: true, y: false })).toBe(false);
         expect(predicate({ x: false, y: true })).toBe(true);
         expect(predicate({ x: false, y: false })).toBe(true);
-
     });
- */
 
+    afterAll(() => {
+        global.console = OLD_CONSOLE;
+    });
+});
 
-    // arithmetic
-    // //     'add', 'sub', 'mul', 'div', 'mod'];
+describe('The OVE filter library - arithmetic operations', () => {
+    const OLD_CONSOLE = global.console;
+    beforeAll(() => {
+        // global.console = { log: jest.fn(x => x), warn: jest.fn(x => x), error: jest.fn(x => x) };
+    });
 
     it('should correctly handle "add" (+)', () => {
         const predicate = getPredicate(parse('(x add 1) eq 5'));
@@ -133,7 +113,6 @@ describe('The OVE filter library', () => {
         expect(predicate({ x: '10' })).toBe(true);
     });
 
-
     it('should correctly handle "mod" (%)', () => {
         const predicate = getPredicate(parse('(x mod 3) eq y'));
         expect(predicate({ x: '0', y: '0' })).toBe(true);
@@ -143,14 +122,152 @@ describe('The OVE filter library', () => {
         expect(predicate({ x: '4', y: '1' })).toBe(true);
     });
 
+    afterAll(() => {
+        global.console = OLD_CONSOLE;
+    });
+});
 
+describe('The OVE filter library - string functions', () => {
+    const OLD_CONSOLE = global.console;
+    beforeAll(() => {
+        // global.console = { log: jest.fn(x => x), warn: jest.fn(x => x), error: jest.fn(x => x) };
+    });
+
+    it('should correctly handle "substringof()"', () => {
+        const predicate = getPredicate(parse('substringof(\'Alfreds\', CompanyName)'));
+        expect(predicate({ CompanyName: 'AlfredsCar' })).toBe(true);
+        expect(predicate({ CompanyName: 'AlfredCar' })).toBe(false);
+    });
+
+    it('should correctly handle "endswith()"', () => {
+        const predicate = getPredicate(parse('endswith(x, \'def\')'));
+        expect(predicate({ x: 'abcdef' })).toBe(true);
+        expect(predicate({ x: 'abcdeg' })).toBe(false);
+    });
+
+    it('should correctly handle "startswith()"', () => {
+        const predicate = getPredicate(parse('startswith(x, \'abc\')'));
+        expect(predicate({ x: 'abcdef' })).toBe(true);
+        expect(predicate({ x: 'ghijkl' })).toBe(false);
+    });
+
+    it('should correctly handle "length()"', () => {
+        const predicate = getPredicate(parse('length(x) eq 3'));
+        expect(predicate({ x: 'abc' })).toBe(true);
+        expect(predicate({ x: 'abcd' })).toBe(false);
+    });
+
+    it('should correctly handle "indexof()"', () => {
+        const predicate = getPredicate(parse('indexof(str, \'abc\') eq pos'));
+        expect(predicate({ str: 'abc', pos: 0 })).toBe(true);
+        expect(predicate({ str: 'abc', pos: 1 })).toBe(false);
+
+        expect(predicate({ str: '.abcd', pos: 1 })).toBe(true);
+    });
+
+    it('should correctly handle "replace()"', () => {
+        const predicate = getPredicate(parse('replace(str, old, replacement) eq new'));
+        expect(predicate({ str: 'car', old: 'ar', replacement: 'ell', new: 'cell' })).toBe(true);
+        expect(predicate({ str: 'car', old: 'ar', replacement: 'ell', new: 'car' })).toBe(false);
+        expect(predicate({ str: 'car', old: 'c', replacement: 'b', new: 'bar' })).toBe(true);
+    });
+
+    /*
+    // The binary substring() function is not currently correctly parse
+    it('should correctly handle "substring()"', () => {
+        const predicate = getPredicate(parse('substring(str, pos) eq res'));
+        expect(predicate({ str: 'abcde', pos: 0, res: 'abcde' })).toBe(true);
+        expect(predicate({ str: 'abcde', pos: 1, res: 'bcde' })).toBe(true);
+    });
+    */
+
+    it('should correctly handle "substring()" with specified length', () => {
+        const predicate = getPredicate(parse('substring(str, pos, len) eq res'));
+        expect(predicate({ str: 'abcde', pos: 0, len: 5, res: 'abcde' })).toBe(true);
+        expect(predicate({ str: 'abcde', pos: 1, len: 3, res: 'bcd' })).toBe(true);
+    });
+
+    it('should correctly handle "tolower()', () => {
+        const predicate = getPredicate(parse('tolower(str) eq res'));
+        expect(predicate({ str: 'AbCd!1', res: 'abcd!1' })).toBe(true);
+        expect(predicate({ str: 'AbCd!1', res: 'abcddd' })).toBe(false);
+    });
+
+    it('should correctly handle "toupper()', () => {
+        const predicate = getPredicate(parse('toupper(str) eq res'));
+        expect(predicate({ str: 'AbCd!1', res: 'ABCD!1' })).toBe(true);
+        expect(predicate({ str: 'AbCd!1', res: 'abcddd' })).toBe(false);
+    });
+
+    it('should correctly handle "trim()', () => {
+        const predicate = getPredicate(parse('trim(str) eq res'));
+        expect(predicate({ str: 'abcd ', res: 'abcd' })).toBe(true);
+        expect(predicate({ str: 'abcd ', res: 'abcd ' })).toBe(false);
+    });
+
+    it('should correctly handle "concat()', () => {
+        const predicate = getPredicate(parse('concat(str1, str2) eq res'));
+        expect(predicate({ str1: 'abcd', str2: 'efg', res: 'abcdefg' })).toBe(true);
+        expect(predicate({ str: 'abcd', str2: 'efg', res: 'abcd' })).toBe(false);
+    });
 
     afterAll(() => {
         global.console = OLD_CONSOLE;
     });
 });
 
+describe('The OVE filter library - math functions', () => {
+    const OLD_CONSOLE = global.console;
+    beforeAll(() => {
+        // global.console = { log: jest.fn(x => x), warn: jest.fn(x => x), error: jest.fn(x => x) };
+    });
+
+    it('should correctly handle "round()"', () => {
+        const predicate = getPredicate(parse('round(x) eq y'));
+        expect(predicate({ x: 0.7, y: 1.0 })).toBe(true);
+        expect(predicate({ x: 1.2, y: 1.0 })).toBe(true);
+        expect(predicate({ x: 1.6, y: 2.0 })).toBe(true);
+    });
+
+    it('should correctly handle "floor()"', () => {
+        const predicate = getPredicate(parse('floor(x) eq y'));
+        expect(predicate({ x: 0.7, y: 0.0 })).toBe(true);
+        expect(predicate({ x: 1.2, y: 1.0 })).toBe(true);
+        expect(predicate({ x: 1.6, y: 1.0 })).toBe(true);
+    });
+
+    it('should correctly handle "ceiling()"', () => {
+        const predicate = getPredicate(parse('ceiling(x) eq y'));
+        expect(predicate({ x: 0.7, y: 1.0 })).toBe(true);
+        expect(predicate({ x: 1.2, y: 2.0 })).toBe(true);
+        expect(predicate({ x: 1.6, y: 2.0 })).toBe(true);
+    });
+
+    afterAll(() => {
+        global.console = OLD_CONSOLE;
+    });
+});
+
+describe('The OVE filter library - handling errors', () => {
+    const OLD_CONSOLE = global.console;
+    beforeAll(() => {
+        global.console = { log: jest.fn(x => x), warn: jest.fn(x => x), error: jest.fn(x => x) };
+    });
+
+    it('test invalid functions throw errors', () => {
+        const predicate = getPredicate({
+            type: 'eq',
+            left: { type: 'functioncall', func: 'NOT_A_REAL_FUNCTION', args: [{ type: 'property', name: 'x' }] },
+            'right': { type: 'property', name: 'y' }
+        });
+        expect(() => predicate({ x: 0.7, y: 0.0 })).toThrow();
+    });
+
+    afterAll(() => {
+        global.console = OLD_CONSOLE;
+    });
+});
 
 // TODO:
-// - invalid operation name (e.g. > rather than gt)
 // - mis-matched parens
+//         const predicate = getPredicate(parse('endswith(x, \'def\''));
